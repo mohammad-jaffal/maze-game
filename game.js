e8c081b0fb49915c92e03a4e77f3c8569e8765f8
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-05-20 20:02:49 +0300

    Cleaning more

--- game.js
+++ game.js
@@ -13,26 +13,74 @@ window.onload = function () {
     var won = false;
     var score = 0;
 
-    
+
 
     // event listener for first time hover over s box
     startBox.addEventListener('mouseenter', function (event) {
         if (firstTime) {
+            statusTitle.innerHTML = "Game Started GL!";
             firstTime = false;
             startGame();
         }
     });
 
-    function colorBorder (color) {
-        for(var i = 0; i < 5; i++){
+
+    // change borders colors for win/lose
+    function colorBorder(color) {
+        for (var i = 0; i < 5; i++) {
             borders[i].style.backgroundColor = color;
         }
     }
 
 
 
-    // restart the game after a win or a loss
-    function restartGame() {
+    // initializing the game for playing
+    function startGame() {
+        colorBorder("#eeeeee");
+
+        // creating on hover for the borders
+        for (var i = 0; i < 5; i++) {
+            // if the mouse touches the borders
+            borders[i].addEventListener('mouseenter', function (event) {
+                if (!lost & !won) {
+                    colorBorder("red");
+                    lost = true;
+                    score -= 10;
+                    statusTitle.innerHTML = `You lost <br/> score: ${score} `;
+                    console.log("lost");
+                    resetGame();
+                }
+            });
+        }
+
+        // when the mouse reaches end without loss
+        endBox.addEventListener('mouseenter', function (event) {
+
+            if (!lost & !won) {
+                won = true;
+                colorBorder("green");
+                score += 5;
+                statusTitle.innerHTML = `You won <br/> score: ${score} `;
+                console.log("won");
+                resetGame();
+            }
+        });
+
+        // button for restarting the game
+        restartBtn.addEventListener('click', function onClick() {
+            location.reload();
+        });
+
+
+    }
+
+
+
+
+
+
+    // reset the game after a win or a loss
+    function resetGame() {
 
         startBox.addEventListener('click', function onClick() {
             if (lost || won) {
@@ -45,44 +93,6 @@ window.onload = function () {
     }
 
 
-    // initializing the game for playing
-    function startGame() {
-        colorBorder("#eeeeee");
-
-        statusTitle.innerHTML = "Begin by moving your mouse over the \"S\"."
-
-        for (var i = 0; i < 5; i++) {
-            // if the mouse touches the borders
-            borders[i].addEventListener('mouseenter', function (event) {
-                if (!lost & !won) {
-                    colorBorder("red");
-                    lost = true;
-                    score -= 10;
-                    statusTitle.innerHTML = `You lost \n score: ${score} `;
-                    console.log("lost");
-                    restartGame();
-                }
-            });
-        }
-
-        // when the mouse reaches end without loss
-        endBox.addEventListener('mouseenter', function (event) {
-
-            if (!lost & !won) {
-                won = true;
-                colorBorder("green");
-                score += 5;
-                statusTitle.innerHTML = `You won \n score: ${score} `;
-                console.log("won");
-                restartGame();
-            }
-        });
-    }
-
-    // button for restarting the game
-    restartBtn.addEventListener('click', function onClick() {
-        location.reload();
-    });
 
 
 }
