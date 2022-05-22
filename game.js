5412d0e87c1f5e6bcd4cb3fa86b81b989e13b6fa
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-05-22 21:28:15 +0300

    Adding live timer

--- game.js
+++ game.js
@@ -12,17 +12,22 @@ window.onload = function () {
     var lost = false;
     var won = false;
     var score = 0;
+    var startTime;
+    var interval
+    var liveTimer;
+    var lastTimer;
+    var bestTimer;
 
     // pre styling restart button
     borders[5].innerHTML = "Restart";
     borders[5].style.textAlign = "center";
     borders[5].style.cursor = "pointer";
-    
+
 
     // adding an empty line under the title for score
     statusTitle.innerHTML = "Begin by moving your mouse over the \"S\".<br/> &#160";
 
-    
+
     // event listener for first time hover over s box
     startBox.addEventListener('mouseenter', function (event) {
         if (firstTime) {
@@ -39,10 +44,11 @@ window.onload = function () {
             borders[i].style.backgroundColor = color;
         }
     }
-    
+
     // function for when user lose 
-    var lostGame = function(event){
+    var lostGame = function (event) {
         if (!lost & !won) {
+            stop();
             colorBorder("red");
             lost = true;
             score -= 10;
@@ -55,6 +61,10 @@ window.onload = function () {
     // function for when user wins 
     var wonGame = function (event) {
         if (!lost & !won) {
+            stop();
+            console.log(liveTimer)
+            // lastTimer = liveTimer;
+            // document.getElementById("liveTime").innerHTML = lastTimer;
             won = true;
             colorBorder("green");
             score += 5;
@@ -63,10 +73,25 @@ window.onload = function () {
         }
     }
 
-   
+
+
+    function stop(){
+        clearInterval(interval);
+    }
+
+
 
     // initializing the game for playing
     function startGame() {
+        startTime  = Date.now();
+        interval = setInterval(function () {
+            liveTimer = Date.now() - startTime;
+            document.getElementById("liveTime").innerHTML = (liveTimer / 1000).toFixed(1);
+        }, 100);
+        
+
+
+
         // resetting boundaries color
         colorBorder("#eeeeee");
 
@@ -105,7 +130,9 @@ window.onload = function () {
         });
     }
 
+    
 
+    
 
 
 }
